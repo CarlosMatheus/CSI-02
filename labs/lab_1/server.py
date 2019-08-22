18dad806654debd4d5f5330be1d73b1ebd41f48e
igor-ribeiiro <igormr98mr@gmail.com>
2019-08-22 20:22:02 -0300

    PUSH

--- labs/lab_1/server.py
+++ labs/lab_1/server.py
@@ -1,5 +1,6 @@
 from SOAPpy import SOAPServer
 
+
 def calcula(op1,op2,operacao):
         if operacao == '+':
                 return op1 + op2
@@ -9,6 +10,8 @@ def calcula(op1,op2,operacao):
                 return op1 * op2
         if operacao == '/':
                 return op1 / op2
+
+
 server = SOAPServer(('localhost',8081))
 server.registerFunction(calcula)
 server.serve_forever()
