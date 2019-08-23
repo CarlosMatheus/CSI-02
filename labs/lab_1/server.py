be8016d8fed4535cfae33f5f78439ae812289da5
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-08-22 21:37:44 -0300

    Fix float and invert F with C

--- labs/lab_1/server.py
+++ labs/lab_1/server.py
@@ -1,17 +1,20 @@
 from SOAPpy import SOAPServer
+from pprint import pprint
 
 
 def convert(temperature, scale):
-        if scale == "F":
-                return {
-                        "temperature": 9/5 * temperature + 32,
-                        "scale": "C"
-                }
-        else:
-                return {
-                        "temperature": (temperature - 32) * 5/9,
+        if scale == "C":
+                response = {
+                        "temperature": 9.0/5.0 * temperature + 32,
                         "scale": "F"
                 }
+        else:
+                response = {
+                        "temperature": (temperature - 32) * 5.0/9.0,
+                        "scale": "C"
+                }
+        pprint(response)
+        return response
 
 
 server = SOAPServer(('localhost', 8081))
