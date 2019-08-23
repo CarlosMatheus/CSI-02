2bc4beb1037d26b53cbd56eddc2a42c4b416d8ed
igorbragaia <igor.bragaia@gmail.com>
2019-08-22 21:21:03 -0300

    gogo

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from SOAPpy import SOAPProxy
 server = SOAPProxy('http://localhost:8081/')
 
 while True:
-    temperature = int(raw_input('Temperature: '))
+    temperature = float(raw_input('Temperature: '))
     scale = raw_input('F or C: ')
     new_temperature = server.convert(temperature, scale)
     print("Response: " + str(new_temperature.temperature) + " " + new_temperature.scale)
